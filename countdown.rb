ef7d16c8b1bc378068bd4b32d4dd0780e77d0fc6
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-04 00:52:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    countdown_with_sleep
+    number -= 1
+    
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  sleep 1
 end
