f7296bb5d8fa38e31722cbb48dbacd94f68ca55b
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-04 01:32:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,8 @@
 
 def countdown(number)
   while number > 0 
-     countdown_with_sleep
     puts "#{number} SECOND(S)!"
-   
+    countdown_with_sleep
     number -= 1
     
   end
@@ -12,5 +11,5 @@ def countdown(number)
 end
 
 def countdown_with_sleep
-  sleep 5
+  sleep 1
 end
